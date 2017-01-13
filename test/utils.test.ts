a7101c75bf484cda5f3a5498b559a9fe3d2bf124
Prashant Tiwari <prashaantt@gmail.com>
2017-01-13 23:47:40 +0530

    Add test for readFileAsync

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,4 +1,5 @@
 import * as assert from 'assert';
+import { resolve } from 'path';
 
 import { parseCoverage, readFileAsync } from '../src/utils';
 
@@ -17,4 +18,13 @@ Lines        : 94.44% ( 51/54 )
 Extra stuff`;
         assert.equal(parseCoverage(coverageReport), 93.50);
     });
+});
+
+suite('readFileAsync', () => {
+
+    test('correctly reads a file asynchronously', async () => {
+
+        const contents = await readFileAsync(resolve(__dirname, '../../test/fixtures/test.txt'));
+        assert.equal(contents, 'Hello, world!\n');
+    })
 });
